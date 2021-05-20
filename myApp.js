e0e71ee3ac4c336ce025822ce0100827010350c8
zenithlel <83914557+zenithlel@users.noreply.github.com>
2021-05-20 10:28:02 +0200

    Create myApp.js

---
+++ myApp.js
@@ -0,0 +1,13 @@
+var express= require('express');
+var app = express();
+
+app.get('/', function(req,res){
+  absulotePath= __dirname + '/views/index.html';
+  res.sendFile(absulotePath);
+})
+
+app.use('/', express.static(__dirname + '/public');
+app.get('json', function(req,res){
+  let jfile = {"message": "Hello Json"};
+  res.send(jfile);
+})
